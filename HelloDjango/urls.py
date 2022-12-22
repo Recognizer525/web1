d37f750b24957ce573ff21f5053baa5faefdb1f8
Recognizer525 <grsurzhikov@yandex.ru>
2022-12-22 17:33:31 +0300

    Version 4

--- HelloDjango/urls.py
+++ HelloDjango/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog import urls as blog_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('blog/', include(blog_urls)),
 ]
+
+handler404 = 'blog.views.handler404'
